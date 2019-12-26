6c1d05dfa16a22cbfbbcac3b61307659c73c742e
Raza haider <raxa.hai@gmail.com>
2019-12-26 18:13:14 +0500

    Fashion_mnist prediction

---
+++ Fashion_MNIST.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import numpy as np 
+import math
+import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
+
+tfds.disable_progress_bar()
+
+dataset,metadata = tfds.load('fashion_mnist',as_supervised=True,with_info=True)
+train_dataset,test_dataset = dataset['train'],dataset['test']
+
+class_names = ['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal', 'Shirt','Sneaker','Bag','Ankle boot']
+
+num_train_examples = metadata.splits['train'].num_examples
+num_test_examples = metadata.splits['test'].num_examples
+print("Number of training examples: {}".format(num_train_examples))
+print("Number of test examples:     {}".format(num_test_examples))
+
+def normalize(images, labels):
+    images = tf.cast(images, tf.float32)
+    images /= 255
+    return images, labels
+
+# The map function applies the normalize function to each element in the train
+# and test datasets
+train_dataset =  train_dataset.map(normalize)
+test_dataset  =  test_dataset.map(normalize)
+
+# The first time you use the dataset, the images will be loaded from disk
+# Caching will keep them in memory, making training faster
+train_dataset =  train_dataset.cache()
+test_dataset  =  test_dataset.cache()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10,  activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+BATCH_SIZE = 32
+train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
+test_dataset = test_dataset.batch(BATCH_SIZE)
+
+history = model.fit(train_dataset, epochs=5, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
+
+test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/32))
+print('Accuracy on test dataset:', test_accuracy)
+
+
+for test_images, test_labels in test_dataset.take(1):
+    test_images = test_images.numpy()
+    test_labels = test_labels.numpy()
+    predictions = model.predict(test_images)
+
+print(predictions[0])
+print(predictions[2])
+print(test_labels[0])
+print(class_names[test_labels[0]])
+print(class_names[test_labels[2]])
