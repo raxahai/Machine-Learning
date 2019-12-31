817ef6055dd192428f7265cb5473c9ee4ed0d30b
Raza haider <raxa.hai@gmail.com>
2019-12-31 18:04:13 +0500

    house prediction : added

--- house_prices.py
+++ house_prices.py
@@ -9,6 +9,7 @@ model.compile(optimizer = "sgd",
 xs = np.array([1.0,2.0,3.0,5.0,8.0,9.0], dtype= float)
 ys = np.array([1.0,1.5,2.0,3.0,4.5,5.0], dtype=float) #scaled down to one decimal place
 # in order to make model work well
-model.fit(xs,ys,epochs = 700)
+model.fit(xs,ys,epochs = 500)
 print(model.predict([7.0]))
-print(model.predict([4.0]))
+print(model.predict([8.0]))
+
